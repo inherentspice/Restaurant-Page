ba6ee0e5a035f3385e78e0ab231b6d929e1ba587
Stephen Smith <stephenlehanesmith@gmail.com>
2022-11-07 14:14:38 +0800

    added section describing the restaurants chef

--- src/home.js
+++ src/home.js
@@ -2,41 +2,45 @@
 export default function loadHome(cached) {
   const mainContent = cached.mainDocumentBody
 
+  const segmentContent = [
+    {
+      "heading": "AI Restaurant",
+      "subHeading": "AI Generated Food in the Heart of New Chusan",
+      "src": "./menu/ai-restaurant-interior.jpg",
+      "alt": "interior of restaurant",
+      "description": "AI Restaurant: God's Favourite Mistake"
+    },
+    {
+      "heading": "Today's Special",
+      "subHeading": "Salmon Carnival Cake",
+      "src": "./menu/salmon-cake.png",
+      "alt": "salmon cake",
+      "description": ""
+    },
+    {
+      "heading": "Meet Our Chef",
+      "subHeading": "Roberto Roboto",
+      "src": "./menu/ai-restaurant-chef.png",
+      "alt": "an ai generated image of a chef",
+      "description": ""
+    }
+  ]
 
-  // restaurant info
-  const restaurantDiv = document.createElement("div")
-  const restaurantHeading = document.createElement("h1")
-  restaurantHeading.textContent = "AI Restaurant"
-  const restaurantSubHeading = document.createElement("h3")
-  restaurantSubHeading.textContent = "AI Generated Food in the Heart of New Chusan"
-  const restaurantPicture = document.createElement("img")
-  restaurantPicture.src = "./menu/ai-restaurant-interior.jpg"
-  restaurantPicture.alt = "interior of a restaurant"
-  const restaurantTitle = document.createElement("h5")
-  restaurantTitle.textContent = "AI Restaurant: God's Favourite Mistake"
-  restaurantDiv.appendChild(restaurantHeading)
-  restaurantDiv.appendChild(restaurantSubHeading)
-  restaurantDiv.appendChild(restaurantPicture)
-  restaurantDiv.appendChild(restaurantTitle)
-  mainContent.appendChild(restaurantDiv)
-
-  // dish of the day
-  const specialDiv = document.createElement("div")
-  const specialHeading = document.createElement("h1")
-  specialHeading.textContent = "Today's Special"
-  const specialSubHeading = document.createElement("h3")
-  specialSubHeading.textContent = "Salmon Carnival Cake"
-  const specialPicture = document.createElement("img")
-  specialPicture.src = "./menu/salmon-cake.png"
-  specialPicture.alt = "salmon cake"
-  const specialDescription = document.createElement("p")
-  specialDescription.textContent = ""
-  specialDiv.appendChild(specialHeading)
-  specialDiv.appendChild(specialSubHeading)
-  specialDiv.appendChild(specialPicture)
-  specialDiv.appendChild(specialDescription)
-  mainContent.appendChild(specialDiv)
-
-  // chef introduction
-
+  segmentContent.forEach((segment) => {
+    const restaurantDiv = document.createElement("div")
+    const restaurantHeading = document.createElement("h1")
+    restaurantHeading.textContent = segment.heading
+    const restaurantSubHeading = document.createElement("h3")
+    restaurantSubHeading.textContent = segment.subHeading
+    const restaurantPicture = document.createElement("img")
+    restaurantPicture.src = segment.src
+    restaurantPicture.alt = segment.alt
+    const restaurantTitle = document.createElement("h5")
+    restaurantTitle.textContent = segment.description
+    restaurantDiv.appendChild(restaurantHeading)
+    restaurantDiv.appendChild(restaurantSubHeading)
+    restaurantDiv.appendChild(restaurantPicture)
+    restaurantDiv.appendChild(restaurantTitle)
+    mainContent.appendChild(restaurantDiv)
+  })
 };
