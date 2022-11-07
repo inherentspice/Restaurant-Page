58dfa908e2ac9e9fe5ade6a41634d2430a9e52a1
Stephen Smith <stephenlehanesmith@gmail.com>
2022-11-07 11:27:43 +0800

    added dish of the day section

--- src/home.js
+++ src/home.js
@@ -1,17 +1,42 @@
 
 export default function loadHome(cached) {
   const mainContent = cached.mainDocumentBody
-  const pageHeading = document.createElement("h1")
-  pageHeading.textContent = "AI Restaurant"
-  const pageSubHeading = document.createElement("h3")
-  pageSubHeading.textContent = "Food for Nobody"
-  const menuItem = document.createElement("img")
-  menuItem.src = "./menu/salmon-cake.png"
-  menuItem.alt = "salmon carnival cake"
-  const menuItemTitle = document.createElement("h5")
-  menuItemTitle.textContent = "Salmon Carnival Cake"
-  mainContent.appendChild(pageHeading)
-  mainContent.appendChild(pageSubHeading)
-  mainContent.appendChild(menuItem)
-  mainContent.appendChild(menuItemTitle)
+
+
+  // restaurant info
+  const restaurantDiv = document.createElement("div")
+  const restaurantHeading = document.createElement("h1")
+  restaurantHeading.textContent = "AI Restaurant"
+  const restaurantSubHeading = document.createElement("h3")
+  restaurantSubHeading.textContent = "AI Generated Food in the Heart of New Chusan"
+  const restaurantPicture = document.createElement("img")
+  restaurantPicture.src = "./menu/ai-restaurant-interior.jpg"
+  restaurantPicture.alt = "interior of a restaurant"
+  const restaurantTitle = document.createElement("h5")
+  restaurantTitle.textContent = "AI Restaurant: God's Favourite Mistake"
+  restaurantDiv.appendChild(restaurantHeading)
+  restaurantDiv.appendChild(restaurantSubHeading)
+  restaurantDiv.appendChild(restaurantPicture)
+  restaurantDiv.appendChild(restaurantTitle)
+  mainContent.appendChild(restaurantDiv)
+
+  // dish of the day
+  const specialDiv = document.createElement("div")
+  const specialHeading = document.createElement("h1")
+  specialHeading.textContent = "Today's Special"
+  const specialSubHeading = document.createElement("h3")
+  specialSubHeading.textContent = "Salmon Carnival Cake"
+  const specialPicture = document.createElement("img")
+  specialPicture.src = "./menu/salmon-cake.png"
+  specialPicture.alt = "salmon cake"
+  const specialDescription = document.createElement("p")
+  specialDescription.textContent = ""
+  specialDiv.appendChild(specialHeading)
+  specialDiv.appendChild(specialSubHeading)
+  specialDiv.appendChild(specialPicture)
+  specialDiv.appendChild(specialDescription)
+  mainContent.appendChild(specialDiv)
+
+  // chef introduction
+
 };
