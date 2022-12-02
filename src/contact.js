c61b745669a59f7443625ddc11889d3891b52eca
Stephen Smith <stephenlehanesmith@gmail.com>
2022-12-02 13:27:21 +0800

    made eslint style changes to code

--- src/contact.js
+++ src/contact.js
@@ -1,7 +1,6 @@
-import footer from "./footer"
+import footer from "./footer";
 
 export default function Contact(cached) {
-  console.log("here")
   const contactPeople = [
     {
       "name": "Roberto Roboto",
@@ -21,38 +20,38 @@ export default function Contact(cached) {
       "phoneNumber": "010-101-1000",
       "email": "l33thack3r@falseemail.com"
     }
-  ]
-  const mainContent = cached.mainDocumentBody
-  const pageHeading = document.createElement("h1")
-  pageHeading.textContent = "Contact Us"
-  pageHeading.className = "contact-heading"
-  const pageSubHeading = document.createElement("h3")
-  pageSubHeading.textContent = "A Being that is Totally Not a Bot Will Get Back to You"
-  pageSubHeading.className = "contact-subheading"
-  mainContent.appendChild(pageHeading)
-  mainContent.appendChild(pageSubHeading)
+  ];
+  const mainContent = cached.mainDocumentBody;
+  const pageHeading = document.createElement("h1");
+  pageHeading.textContent = "Contact Us";
+  pageHeading.className = "contact-heading";
+  const pageSubHeading = document.createElement("h3");
+  pageSubHeading.textContent = "A Being that is Totally Not a Bot Will Get Back to You";
+  pageSubHeading.className = "contact-subheading";
+  mainContent.appendChild(pageHeading);
+  mainContent.appendChild(pageSubHeading);
 
-  const contactPeopleDiv = document.createElement("div")
-  contactPeopleDiv.className = "contact-segment"
+  const contactPeopleDiv = document.createElement("div");
+  contactPeopleDiv.className = "contact-segment";
   contactPeople.forEach((person) => {
-    const singlePersonDiv = document.createElement("div")
-    singlePersonDiv.className = "contact-cont"
-    const personName = document.createElement("p")
-    personName.textContent = person.name
-    const personRole = document.createElement("p")
-    personRole.textContent = person.role
-    const personNumber = document.createElement("p")
-    personNumber.textContent = person.phoneNumber
-    const personEmail = document.createElement("p")
-    personEmail.textContent = person.email
-    singlePersonDiv.appendChild(personName)
-    singlePersonDiv.appendChild(personRole)
-    singlePersonDiv.appendChild(personNumber)
-    singlePersonDiv.appendChild(personEmail)
-    contactPeopleDiv.appendChild(singlePersonDiv)
-  })
+    const singlePersonDiv = document.createElement("div");
+    singlePersonDiv.className = "contact-cont";
+    const personName = document.createElement("p");
+    personName.textContent = person.name;
+    const personRole = document.createElement("p");
+    personRole.textContent = person.role;
+    const personNumber = document.createElement("p");
+    personNumber.textContent = person.phoneNumber;
+    const personEmail = document.createElement("p");
+    personEmail.textContent = person.email;
+    singlePersonDiv.appendChild(personName);
+    singlePersonDiv.appendChild(personRole);
+    singlePersonDiv.appendChild(personNumber);
+    singlePersonDiv.appendChild(personEmail);
+    contactPeopleDiv.appendChild(singlePersonDiv);
+  });
 
-  mainContent.appendChild(contactPeopleDiv)
+  mainContent.appendChild(contactPeopleDiv);
 
-  footer(cached)
+  footer(cached);
 }
--- src/home.js
+++ src/home.js
@@ -1,9 +1,9 @@
-import loadMenu from "./menu.js"
-import reload from "./reload.js"
-import footer from "./footer.js"
+import loadMenu from "./menu.js";
+import reload from "./reload.js";
+import footer from "./footer.js";
 
 export default function loadHome(cached) {
-  const mainContent = cached.mainDocumentBody
+  const mainContent = cached.mainDocumentBody;
 
   const imageDivData = [
     {
@@ -22,110 +22,110 @@ export default function loadHome(cached) {
       "src": "./menu/poison-cocktail.png",
       "text": "poison cocktail"
     }
-  ]
+  ];
 
-  const leadImageDiv = document.createElement("div")
-  leadImageDiv.className = "lead-image-cont"
-  const leadImageTextTitle = document.createElement("p")
-  leadImageTextTitle.textContent = "Existential Dread Pancakes"
-  leadImageTextTitle.className = "lead-image-title lead-image"
-  const leadImageSubtitle = document.createElement("p")
-  leadImageSubtitle.textContent = "Mouth-Watering Pancakes with a Dollop of Dread"
-  leadImageSubtitle.className = "lead-image-subtitle lead-image"
-  const menuButton = document.createElement("button")
-  menuButton.className = "lead-image-menu-btn"
-  menuButton.innerText = "Click here for more menu items"
+  const leadImageDiv = document.createElement("div");
+  leadImageDiv.className = "lead-image-cont";
+  const leadImageTextTitle = document.createElement("p");
+  leadImageTextTitle.textContent = "Existential Dread Pancakes";
+  leadImageTextTitle.className = "lead-image-title lead-image";
+  const leadImageSubtitle = document.createElement("p");
+  leadImageSubtitle.textContent = "Mouth-Watering Pancakes with a Dollop of Dread";
+  leadImageSubtitle.className = "lead-image-subtitle lead-image";
+  const menuButton = document.createElement("button");
+  menuButton.className = "lead-image-menu-btn";
+  menuButton.innerText = "Click here for more menu items";
   menuButton.addEventListener("click", () => {
-    reload(cached)
-    loadMenu(cached)
-  })
-  leadImageDiv.appendChild(leadImageTextTitle)
-  leadImageDiv.appendChild(leadImageSubtitle)
-  leadImageDiv.appendChild(menuButton)
-  mainContent.appendChild(leadImageDiv)
+    reload(cached);
+    loadMenu(cached);
+  });
+  leadImageDiv.appendChild(leadImageTextTitle);
+  leadImageDiv.appendChild(leadImageSubtitle);
+  leadImageDiv.appendChild(menuButton);
+  mainContent.appendChild(leadImageDiv);
 
   // create div that describes the restaurant
-  const restaurantDiv = document.createElement("div")
-  restaurantDiv.className = "home-segment alt"
-  const restaurantImageDiv = document.createElement("div")
-  restaurantImageDiv.className = "image-cont"
-  const restaurantImage = document.createElement("img")
-  restaurantImage.src = "./menu/ai-restaurant-interior.png"
-  const restaurantDescriptionDiv = document.createElement("div")
-  restaurantDescriptionDiv.className = "description-cont"
-  const restaurantHeading = document.createElement("h1")
-  restaurantHeading.textContent = "AI Restaurant"
-  const restaurantSubHeading = document.createElement("h3")
-  restaurantSubHeading.textContent = "Proudly Serving AI Generated Food in the Heart of New Chusan since 2024"
-  const restaurantText = document.createElement("p")
-  restaurantText.textContent = "We've been pushing the limits of what can be considered food for over twenty years."
-  const restaurantTextParaTwo = document.createElement("p")
+  const restaurantDiv = document.createElement("div");
+  restaurantDiv.className = "home-segment alt";
+  const restaurantImageDiv = document.createElement("div");
+  restaurantImageDiv.className = "image-cont";
+  const restaurantImage = document.createElement("img");
+  restaurantImage.src = "./menu/ai-restaurant-interior.png";
+  const restaurantDescriptionDiv = document.createElement("div");
+  restaurantDescriptionDiv.className = "description-cont";
+  const restaurantHeading = document.createElement("h1");
+  restaurantHeading.textContent = "AI Restaurant";
+  const restaurantSubHeading = document.createElement("h3");
+  restaurantSubHeading.textContent = "Proudly Serving AI Generated Food in the Heart of New Chusan since 2024";
+  const restaurantText = document.createElement("p");
+  restaurantText.textContent = "We've been pushing the limits of what can be considered food for over twenty years.";
+  const restaurantTextParaTwo = document.createElement("p");
   restaurantTextParaTwo.textContent = "Whether it be skirting perfection with our Salmon Carnival Cake, or defying \
   the laws of god and man with our signature Narwal Surprise Burger: we gaurantee that when you leave AI Restaurant, \
-  you leave with a story."
-  restaurantImageDiv.appendChild(restaurantImage)
-  restaurantDescriptionDiv.appendChild(restaurantHeading)
-  restaurantDescriptionDiv.appendChild(restaurantSubHeading)
-  restaurantDescriptionDiv.appendChild(restaurantText)
-  restaurantDescriptionDiv.appendChild(restaurantTextParaTwo)
-  restaurantDiv.appendChild(restaurantImageDiv)
-  restaurantDiv.appendChild(restaurantDescriptionDiv)
-  mainContent.appendChild(restaurantDiv)
+  you leave with a story.";
+  restaurantImageDiv.appendChild(restaurantImage);
+  restaurantDescriptionDiv.appendChild(restaurantHeading);
+  restaurantDescriptionDiv.appendChild(restaurantSubHeading);
+  restaurantDescriptionDiv.appendChild(restaurantText);
+  restaurantDescriptionDiv.appendChild(restaurantTextParaTwo);
+  restaurantDiv.appendChild(restaurantImageDiv);
+  restaurantDiv.appendChild(restaurantDescriptionDiv);
+  mainContent.appendChild(restaurantDiv);
 
   // create div display of restaurants drinks
-  const drinkImagesDiv = document.createElement("div")
-  drinkImagesDiv.className = "drink-images-cont"
+  const drinkImagesDiv = document.createElement("div");
+  drinkImagesDiv.className = "drink-images-cont";
 
   imageDivData.forEach((image, index) => {
-    const drinkImageDiv = document.createElement("div")
-    drinkImageDiv.className = `drink-image-cont box-${index}`
-    const drinkImage = document.createElement("img")
-    drinkImage.src = image.src
-    const drinkText = document.createElement("p")
-    drinkText.className = "drink-text"
-    drinkText.textContent = image.text
-    drinkImageDiv.appendChild(drinkImage)
-    drinkImageDiv.appendChild(drinkText)
-    drinkImagesDiv.appendChild(drinkImageDiv)
-    mainContent.appendChild(drinkImagesDiv)
-  })
+    const drinkImageDiv = document.createElement("div");
+    drinkImageDiv.className = `drink-image-cont box-${index}`;
+    const drinkImage = document.createElement("img");
+    drinkImage.src = image.src;
+    const drinkText = document.createElement("p");
+    drinkText.className = "drink-text";
+    drinkText.textContent = image.text;
+    drinkImageDiv.appendChild(drinkImage);
+    drinkImageDiv.appendChild(drinkText);
+    drinkImagesDiv.appendChild(drinkImageDiv);
+    mainContent.appendChild(drinkImagesDiv);
+  });
 
   // event listener so that on click the drinks go from stacked on top
   // to laid out in a line
   drinkImagesDiv.addEventListener("click", () => {
     if (drinkImagesDiv.className !== "drink-images-cont spread") {
-      drinkImagesDiv.className += " spread"
-      drinkImagesDiv.childNodes.forEach((item) => item.className += " spread")
+      drinkImagesDiv.className += " spread";
+      drinkImagesDiv.childNodes.forEach((item) => item.className += " spread");
     }
-  })
+  });
 
 
 
   // create div of information about the restaurant's chef
-  const chefDiv = document.createElement("div")
-  chefDiv.className = "home-segment alt"
-  const chefImageDiv = document.createElement("div")
-  chefImageDiv.className = "image-cont"
-  const chefImage = document.createElement("img")
-  chefImage.src = "./menu/ai-restaurant-chef.png"
-  const chefDescriptionDiv = document.createElement("div")
-  chefDescriptionDiv.className = "description-cont"
-  const chefHeading = document.createElement("h1")
-  chefHeading.textContent = "Meet Our Chef"
-  const chefSubHeading = document.createElement("h3")
-  chefSubHeading.textContent = "Roberto Roboto"
-  const chefText = document.createElement("p")
+  const chefDiv = document.createElement("div");
+  chefDiv.className = "home-segment alt";
+  const chefImageDiv = document.createElement("div");
+  chefImageDiv.className = "image-cont";
+  const chefImage = document.createElement("img");
+  chefImage.src = "./menu/ai-restaurant-chef.png";
+  const chefDescriptionDiv = document.createElement("div");
+  chefDescriptionDiv.className = "description-cont";
+  const chefHeading = document.createElement("h1");
+  chefHeading.textContent = "Meet Our Chef";
+  const chefSubHeading = document.createElement("h3");
+  chefSubHeading.textContent = "Roberto Roboto";
+  const chefText = document.createElement("p");
   chefText.textContent = "Roberto attended cooking school in France at the Maulin De Morgan's Restaurant \
   on the now disappeared French Riviera. Now, he was taken New Chusan by storm. If he isn't plugged into \
   his designated Cool-Tech brand charging station, you'll find him behind the stove, creating affronts \
-  to nature / dreams."
-  chefImageDiv.appendChild(chefImage)
-  chefDescriptionDiv.appendChild(chefHeading)
-  chefDescriptionDiv.appendChild(chefSubHeading)
-  chefDescriptionDiv.appendChild(chefText)
-  chefDiv.appendChild(chefDescriptionDiv)
-  chefDiv.appendChild(chefImageDiv)
-  mainContent.appendChild(chefDiv)
+  to nature / dreams.";
+  chefImageDiv.appendChild(chefImage);
+  chefDescriptionDiv.appendChild(chefHeading);
+  chefDescriptionDiv.appendChild(chefSubHeading);
+  chefDescriptionDiv.appendChild(chefText);
+  chefDiv.appendChild(chefDescriptionDiv);
+  chefDiv.appendChild(chefImageDiv);
+  mainContent.appendChild(chefDiv);
 
-  footer(cached)
-};
+  footer(cached);
+}
